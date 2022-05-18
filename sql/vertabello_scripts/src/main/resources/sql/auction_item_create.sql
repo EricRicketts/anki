2b357eb4842548817beb757b92c86e05d3214109
Eric Ricketts <eric_ricketts@me.com>
2022-05-18 18:08:55 -0400

    auction and bidder tables

--- sql/vertabello_scripts/src/main/resources/sql/auction_item_create.sql
+++ sql/vertabello_scripts/src/main/resources/sql/auction_item_create.sql
@@ -1,18 +1,19 @@
 use vertabello_recap_of_sql_joins;
-create table if not exists bidder (
-    id int not null,
+create table if not exists auction_item (
+    id int primary key auto_increment not null,
     name varchar(100) not null,
-    bought_item_id int,
-    final_price int not null,
-    foreign key (bought_item_id) references auction_item(id) on delete cascade
+    type varchar(50) not null,
+    initial_price int
 );
 
-insert into bidder(id, name, bought_item_id, final_price)
-    values (1, 'Carlos Santorini', 1, 6750),
-    (1, 'Carlos Santorini', 4, 1100),
-    (2, 'Michael Yung', 3, 400),
-    (2, 'Michael Yung', 2, 760),
-    (3, 'Sylvia Bartoletti', 7, 2200),
-    (4, 'Katherine Manner', 10, 24000),
-    (5, 'Sven Ymmirson', 9, 5000),
-    (6, 'Peter Prevny', 8, 130);
+insert into auction_item(id, name, type, initial_price)
+    values (1, 'Across the Stars', 'painting', 5000),
+    (2, 'Separated Lovers', 'sculpture', 400),
+    (3, 'Liberated David', 'manuscript', 120),
+    (4, 'Three Apples', 'painting', 760),
+    (5, 'Temptation', 'sculpture', 320),
+    (6, 'Thinking Thomas', 'book', 450),
+    (7, 'Libretto', 'manuscript', 670),
+    (8, 'Theology and Divinity', 'book', 130),
+    (9, 'Sun and Moon', 'painting', 3000),
+    (10, 'Behind the Weil', 'painting', 6000);
