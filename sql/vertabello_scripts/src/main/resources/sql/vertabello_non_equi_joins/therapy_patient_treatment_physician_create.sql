2a5c62257c6cec61b7d57dc827072d1e44764a11
Eric Ricketts <eric_ricketts@me.com>
2022-05-31 10:17:27 -0400

    snapshot of tables for equi-joins

--- sql/vertabello_scripts/src/main/resources/sql/vertabello_non_equi_joins/therapy_patient_treatment_physician_create.sql
+++ sql/vertabello_scripts/src/main/resources/sql/vertabello_non_equi_joins/therapy_patient_treatment_physician_create.sql
@@ -22,3 +22,43 @@ values (1, 'mark.jacobson112@outlook.com', 'Mark', 'Jacobson'),
     (13, 'david.phillips515@gmail.com', 'David', 'Phillips'),
     (14, 'super.gamer77@tank.com', 'William', 'Jones'),
     (15, 'briankillerofthefox@hotmail.com', 'Brian', 'Fox');
+
+create table if not exists physician (
+    id int primary key auto_increment not null,
+    email varchar(255) not null,
+    first_name varchar(100) not null,
+    last_name varchar(100) not null,
+    experience int not null
+);
+
+insert into physician(id, email, first_name, last_name, experience)
+values (1, 'henry.rhoads@medicalcenter.com', 'Henry', 'Rhoads', 4),
+    (2, 'b.harper67@me.com', 'Brandon', 'Harper', 28),
+    (3, 'calderwood.john@doctorhelp.ch', 'John', 'Calderwood', 7),
+    (4, 'rubygriffin23@hotmail.com', 'Ruby', 'Griffin', 8),
+    (5, 'vernon.ch86@doctorhelp.ch', 'Vernon', 'Chambers',  5),
+    (6, 'donaldblunt@medicalcenter.com', 'Donald', 'Blunt', 25),
+    (7, 'billycollins111@hotmail.com', 'Billy', 'Collins', 7),
+    (8, 'jferris@me.com', 'James', 'Ferris', 10),
+    (9, 'sam.orange@medicalcenter.com', 'Samantha', 'Core', 13),
+    (10, 'ige.kuri@hotmail.com', 'Ige', 'Kuri', 1);
+
+create table if not exists treatment (
+    id int primary key auto_increment not null,
+    name varchar(100) not null,
+    price decimal(4, 1) not null,
+    type varchar(255) not null
+);
+
+insert into treatment(id, name, price, type)
+values (1, 'Thermotherapy', 96.5, 'pain relief'),
+    (2, 'Cryotherapy', 114.0, 'treatment of tissue damage'),
+    (3, 'Laser therapy', 338.0, 'laser medicine'),
+    (4, 'Water cure', 60.5, 'relaxation'),
+    (5, 'Thai massage', 67.2, 'acupressure'),
+    (6, 'Shiatsu', 85.0, 'acupressure'),
+    (7, 'Aromatherapy', 55.0, 'relaxation'),
+    (8, 'Hypnotherapy', 159.0, 'relaxation'),
+    (9, 'Hot stone massage', 60.0, 'relaxation'),
+    (10, 'Acupuncture', 115.0, 'acupressure'),
+    (11, 'Chiropractic', 199.0, 'pain relief');
